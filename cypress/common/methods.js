910230929b233c9ba8d17c2d16addd62a978168c
agustin <agustincatalano@gmail.com>
2022-01-09 19:06:38 +0100

    importing methods

--- cypress/common/methods.js
+++ cypress/common/methods.js
@@ -1,4 +1,24 @@
-export const methods = {
+import * as $ from '../common/selectors'
 
-    
+export const methods = {
+  createNewBoard(boardName) {
+    //Create new board
+    cy.get($.activeBoardSelector).should('not.exist')
+    cy.get($.newBoardSelector).click()
+    cy.get($.activeBoardSelector).should('be.visible')
+    cy.get($.boardInput).type(boardName)
+    cy.get($.saveButton).click()
+    cy.url().should('include', 'board')
+    cy.get($.boardContainer).should('be.visible')
+  },
+
+  addLists(numberOfLists) {
+    for (let i = 0; i < numberOfLists; i++) {
+      cy.get($.addListSelector).click()
+      cy.get($.listInputSelector).type(`List ${i}`)
+      cy.get($.saveButtonSelector).click()
+    }
+    //validate lists are created
+    cy.get('[data-cy="list"]').should('have.length', numberOfLists)
+  }
 }
--- cypress/integration/board.spec.js
+++ cypress/integration/board.spec.js
@@ -1,6 +1,7 @@
 import * as $ from '../common/selectors'
+import { methods } from '../common/methods'
 
-export const boardName = 'AWESOME BOARD'
+const boardName = 'AWESOME BOARD'
 const numberOfLists = 3
 
 before(() => {
@@ -24,7 +25,9 @@ describe('Home page validations', () => {
       .should('be.visible')
       .contains('Log in')
       .should('have.css', 'background-color', 'rgb(41, 143, 202)')
-    cy.get($.loginButtonSelector).find('svg').should('have.css', 'width', '20px')
+    cy.get($.loginButtonSelector)
+      .find('svg')
+      .should('have.css', 'width', '20px')
 
     cy.get($.loginButtonSelector)
       .find('svg')
@@ -34,8 +37,13 @@ describe('Home page validations', () => {
       })
     //body
     cy.get($.bodyContainerSelector).should('be.visible')
-    cy.get($.boardTitleSelector).eq(1).should('be.visible').contains('My Boards')
-    cy.get($.newBoardSelector).should('be.visible').contains('Create a board...')
+    cy.get($.boardTitleSelector)
+      .eq(1)
+      .should('be.visible')
+      .contains('My Boards')
+    cy.get($.newBoardSelector)
+      .should('be.visible')
+      .contains('Create a board...')
     cy.get($.newBoardSelector).should(
       'have.css',
       'background-color',
@@ -46,24 +54,11 @@ describe('Home page validations', () => {
 
 describe('Create/update Board and lists', () => {
   it(`Create a New Board with name ${boardName}`, () => {
-    //Create new board
-    cy.get($.activeBoardSelector).should('not.exist')
-    cy.get($.newBoardSelector).click()
-    cy.get($.activeBoardSelector).should('be.visible')
-    cy.get($.boardInput).type(boardName)
-    cy.get($.saveButton).click()
-    cy.url().should('include', 'board')
-    cy.get($.boardContainer).should('be.visible')
+    methods.createNewBoard(boardName)
   })
 
   it(`Add ${numberOfLists} lists`, () => {
-    for (let i = 0; i < numberOfLists; i++) {
-      cy.get($.addListSelector).click()
-      cy.get($.listInputSelector).type(`List ${i}`)
-      cy.get($.saveButtonSelector).click()
-    }
-    //validate lists are created
-    cy.get('[data-cy="list"]').should('have.length', numberOfLists)
+    methods.addLists(numberOfLists)
   })
 
   it('Delete all list in the current board', () => {
@@ -77,6 +72,9 @@ describe('Create/update Board and lists', () => {
     cy.get($.boardOptionsSelector).click()
     cy.get($.deleteBoardSelector).eq(1).click()
     cy.get($.bodyContainerSelector).should('be.visible')
-    cy.get($.boardTitleSelector).eq(1).should('be.visible').contains('My Boards')
+    cy.get($.boardTitleSelector)
+      .eq(1)
+      .should('be.visible')
+      .contains('My Boards')
   })
 })
